c64c7f44e88a23d19a7121aea0713417a2d70643
finalSay9 <chimwazaevan1@gmail.com>
2026-04-05 14:27:22 +0200

    adding some root

--- Binary_tree.py
+++ Binary_tree.py
@@ -6,15 +6,15 @@ class Node:
         
 
 class BinarySearchTree():
-    #terms
-    #Node, root_node
-    #left_child, right_child, parent
-    #right_sub_tree, right_sub_tree
-    def __init__(self,root_node, node):
-        self.node = node
-        self.root_node = root_node
-    
-    def leftChild(self,left_child):
+  
+    def __init__(self,root):
+        self.node = root
+
+
+    def insert(self, key):
+        if self.root == None:
+            self.root = Node(key)
+
 
 
        
