4bb441196a879ef2e728a9f50db094c06976fd32
finalSay9 <chimwazaevan1@gmail.com>
2026-04-05 14:33:31 +0200

    creating a pvt _recursive function to insert the node/key

--- Binary_tree.py
+++ Binary_tree.py
@@ -14,6 +14,13 @@ class BinarySearchTree():
     def insert(self, key):
         if self.root == None:
             self.root = Node(key)
+        else: self._insert_recursive(self.root,key)
+
+    def _insert_recursive(self, current, key):
+        if key < current.key:
+            if current.left == None:
+                current.left = None(key)
+
 
 
 
