74bf1ca08d9c6fbb62e20f5f87b4858d0b9a5caf
finalSay9 <chimwazaevan1@gmail.com>
2026-04-05 14:04:33 +0200

    implementing the node class

--- Binary_tree.py
+++ Binary_tree.py
@@ -1,6 +1,20 @@
-class BinaryTree():
-    def __init__(self,root,left,right):
-        self.root = root
-        self.left = left
-        self.right = right
+class Node:
+    def __init__(self, key):
+        self.key = key
+        self.left = None
+        self.right = None
         
+
+class BinarySearchTree():
+    #terms
+    #Node, root_node
+    #left_child, right_child, parent
+    #right_sub_tree, right_sub_tree
+    def __init__(self,root_node, node):
+        self.node = node
+        self.root_node = root_node
+    
+    def leftChild(self,left_child):
+
+
+       
