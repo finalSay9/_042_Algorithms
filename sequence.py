7a22db55a1a578efdbbcd46107def2c349cb1e6a
finalSay9 <chimwazaevan1@gmail.com>
2026-04-07 11:24:03 +0200

    commiting uncommitted changes

--- sequence.py
+++ sequence.py
@@ -1,11 +1,12 @@
-#creating a sequence for finding the fibonacci sequence
-
-def fib(number):
-    a = 0
-    b = 1
-    if number == 1:
-        return number
-    else: 
-        fib(number -1) + fib(number + 1)
+def fib(n):
+    a=0
+    b=1
+    
+    if n == 0:
+        return a
+    elif n == 1:
+        return b
+    a,b = b, (a+b)
+    return a, b
 
 print(fib(4))
