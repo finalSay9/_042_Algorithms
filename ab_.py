e08608cc39dfa60c9ca554b90e30822dc5657951
finalSay9 <chimwazaevan1@gmail.com>
2026-04-08 22:16:14 +0200

    creating an algorithm to find the missing number in a sequence

--- ab_.py
+++ ab_.py
@@ -1,8 +1,15 @@
-num = 12345
-reversed_num = 0
+def find_missing(nums):
+    diff = nums[1] - nums[0]
+    missing = []
 
-while num > 0:
-    digit = num % 10
-    reversed_num = (reversed_num * 10) + digit
-    num = num // 10
-print(reversed_num)
+    for i in range(len(nums) - 1):
+        current = nums[i]
+        while nums[i+1] - current > diff:
+            current += diff
+            missing.append(current)
+        
+    return missing
+        
+
+nums = [1,2,3,5,6,8]
+print(find_missing(nums))
